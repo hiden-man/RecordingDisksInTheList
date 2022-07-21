d17b495d3f0324ebce6ea76b7bf57fdd1e310e3f
Hidenman <elderlutique@gmail.com>
2022-07-21 21:32:48 +0300

    Добавьте файлы проекта.

---
+++ RecordingDisksInTheList/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RecordingDisksInTheList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // запис всіх існуючих дисків у системі в список
+            DriveInfo[] ListDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in ListDrives)
+                if (driveInfo.IsReady)
+                    comboBox1.Items.Add(driveInfo.Name);
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
